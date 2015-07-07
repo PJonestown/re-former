f8616322aad7f8b6b477749533610dc31bffdfbe
Newton <anthpaul0@gmail.com>
2015-07-07 13:11:37 -0500

    Added users#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,18 @@
 class UsersController < ApplicationController
   def new
-
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path
+      redirect_to user_path(@user)
     else
       render :new
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
